e415c4f88071f6fb34f6d291addf91953f3bc0b1
mcdobr <mircea.dobreanu@gmail.com>
2020-06-01 21:56:21 +0300

    Change folder to be on root of bucket

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 source /home/mircea/.zshrc
 rm -rf ./dist
 ng build --prod --aot
-aws s3 cp ./dist ${CC_FRONTEND_S3_BUCKET_URL} --recursive
+aws s3 cp ./dist/frontend ${CC_FRONTEND_S3_BUCKET_URL} --recursive
