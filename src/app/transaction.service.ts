3c609e558c40f77b185fe7e8da8df6d7e29ef4d9
mcdobr <mircea.dobreanu@gmail.com>
2021-03-28 01:58:31 +0200

    Make ndjson streaming work by working the stream manually

--- src/app/transaction.service.ts
+++ src/app/transaction.service.ts
@@ -3,6 +3,9 @@ import {HttpClient} from '@angular/common/http';
 import {Transaction} from './transaction';
 import {environment} from '../environments/environment';
 import {Observable} from 'rxjs';
+import {ndjsonStream} from 'can-ndjson-stream';
+import {OAuthService} from 'angular-oauth2-oidc';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,40 +13,55 @@ import {Observable} from 'rxjs';
 export class TransactionService {
   apiUrl = environment.apiBaseUri;
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient, private oAuthService: OAuthService) {
   }
 
-  getTransactionStream(): Observable<Transaction[]> {
+  // getTransactionStream(): Observable<Transaction[]> {
+  getTransactionStream(): any {
     // return this.httpClient.get<Transaction[]>(this.getAllTransactionsUrl);
     const getAllTransactionsUrl = `${this.apiUrl}/transactions`;
-    return new Observable<Transaction[]>((observer) => {
-      const eventSource = new EventSource(getAllTransactionsUrl);
-      eventSource.onmessage = (event) => {
-        console.log('Received event: ', event);
-        const json = JSON.parse(event.data);
-        observer.next([
-            new Transaction(
-              json['id'],
-              json['sum'],
-              json['otherParty'],
-              json['type'],
-              json['createdAt'],
-              json['lastModifiedAt'],
-              json['description']
-            )
-          ]
-        );
-      };
-      eventSource.onerror = (error) => {
-        if (eventSource.readyState === 0) {
-          console.log('The stream has been closed by the server.');
-          eventSource.close();
-          observer.complete();
-        } else {
-          observer.error('EventSource error: ' + error);
-        }
-      };
-    });
+
+    let transactions: Transaction[];
+
+
+    const obs: Observable<Transaction[]> = new Observable();
+
+
+    fetch(getAllTransactionsUrl, {
+      headers: {
+        Accept: 'application/x-ndjson',
+        Authorization: `Bearer ${this.oAuthService.getAccessToken()}`
+      }
+    })
+      .then(response => {
+        console.log(response.body);
+        // const stream = ndjsonStream(response.body);
+        // console.log(stream);
+        // return stream;
+        return response.body;
+      })
+      .then(stream => {
+        let read;
+        const reader = stream.getReader();
+        reader.read().then(read = (result) => {
+          if (result.done) {
+            console.log('Done parsing ndjson stream of transactions.');
+            return;
+          }
+
+          const decodedText = new TextDecoder().decode(result.value);
+          const tx = decodedText.split('\n')
+            .filter(str => str !== '').map(str => JSON.parse(str));
+          console.log(tx);
+
+          obs.subscribe(subscriber => {
+
+          });
+
+          reader.read().then(read); // recurse through the stream
+        });
+      });
+    // return new Observable();
   }
 
   create(expense: any) {
