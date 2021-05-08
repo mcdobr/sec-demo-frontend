a9d36e509ba481c52d392f9faeb4e3c0a3ed2c80
mcdobr <mircea.dobreanu@gmail.com>
2021-05-08 13:16:39 +0300

    Move OAuth2 config to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import {OAuthService} from 'angular-oauth2-oidc';
+import {Component} from '@angular/core';
+import {OAuthErrorEvent, OAuthService} from 'angular-oauth2-oidc';
 import {authConfig} from './sso.config';
 import {JwksValidationHandler} from 'angular-oauth2-oidc-jwks';
 
@@ -10,4 +10,24 @@ import {JwksValidationHandler} from 'angular-oauth2-oidc-jwks';
 })
 export class AppComponent {
   title = 'Expensy';
+
+  constructor(private oAuthService: OAuthService) {
+    this.configure();
+  }
+
+  private configure() {
+    // Debugging purposes
+    this.oAuthService.events.subscribe(event => {
+      if (event instanceof OAuthErrorEvent) {
+        console.error('OAuthError Event Object:', event);
+      } else {
+        console.warn('Oauth2Event: ', event);
+      }
+    });
+
+    this.oAuthService.configure(authConfig);
+    this.oAuthService.setupAutomaticSilentRefresh();
+    this.oAuthService.tokenValidationHandler = new JwksValidationHandler();
+    this.oAuthService.loadDiscoveryDocumentAndTryLogin();
+  }
 }
--- src/app/authentication-bar/authentication-bar.component.ts
+++ src/app/authentication-bar/authentication-bar.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {OAuthService} from 'angular-oauth2-oidc';
-import {authConfig} from '../sso.config';
-import {JwksValidationHandler} from 'angular-oauth2-oidc-jwks';
 import {Router} from '@angular/router';
 
 @Component({
@@ -13,13 +11,6 @@ export class AuthenticationBarComponent implements OnInit {
 
   constructor(public oAuthService: OAuthService,
               private router: Router) {
-    this.configure();
-  }
-
-  private configure() {
-    this.oAuthService.configure(authConfig);
-    this.oAuthService.tokenValidationHandler = new JwksValidationHandler();
-    this.oAuthService.loadDiscoveryDocumentAndTryLogin();
   }
 
   ngOnInit(): void {
